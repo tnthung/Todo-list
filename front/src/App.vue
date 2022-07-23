f57fa89473481f3bffe8d895881099b3a26c6d40
TNThung <hung11112002@gmail.com>
2022-07-23 12:19:25 +0000

    [Change] remove testing code [Change] remove dup style

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,35 +1,16 @@
 <script setup lang="ts">
 
-import { ref } from "vue";
-
 import useDarkmode   from "./stores/darkmode";
-import MenuComponent from "./components/MenuComponent.vue";
-
 const darkmode = useDarkmode();
-
-const x = ref(0);
-const y = ref(0);
-
-const place = () => {
-  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-  x.value = vw-215;
-  y.value = 15;
-};
-
-place();
-addEventListener("resize", () => place());
 
 </script>
 
 
 <template>
-  <div id="page" :class="darkmode.class">
-    <router-view class="main"/>
-    <MenuComponent 
-      :style="`--x: ${x}px; --y: ${y}px;`"
-      class="menu"
-    />
-  </div>
+  <router-view  
+    :class="darkmode.class"
+    id="page" 
+  />
 </template>
 
 
@@ -40,20 +21,9 @@ addEventListener("resize", () => place());
   inset: 0 0 0 0;
 
   font-family: 'Roboto', sans-serif;
-  color: var(--font1);
+  color: var(--fontColor1);
 
   background-color: var(--bg1);
-}
-
-.menu {
-  position: absolute;
-  top : var(--y);
-  left: var(--x);
-}
-
-.main {
-  position: absolute;
-  inset: 0 0 0 0;
 }
 
 </style>
