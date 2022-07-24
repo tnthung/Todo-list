5304928592bac1d73b32371fc2e1d204aa11a8e5
TNThung <hung11112002@gmail.com>
2022-07-24 11:32:10 +0000

    [Change] refactor

--- front/src/stores/darkmode.ts
+++ front/src/stores/darkmode.ts
@@ -5,25 +5,23 @@ export default defineStore({
   id: "darkmode",
 
   state: () => {
-    const dark = 
+    const dark =
       localStorage.getItem("Todoo.dark") === "true" ??
       window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    return ({ 
-      value: dark,
-    });
+    return { value: dark };
   },
 
   getters: {
     class: (s) => s.value ? "dark" : "light",
   },
-  
+
   actions: {
-    toggle() { 
+    toggle() {
       this.value = !this.value;
 
       localStorage.setItem(
-        "Todoo.dark", 
+        "Todoo.dark",
         this.value.toString()
       );
     },
