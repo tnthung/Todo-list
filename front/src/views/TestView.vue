bd29c6e5f837bd9661625949bd78bb364e650b92
TNThung <hung11112002@gmail.com>
2022-07-25 12:52:29 +0000

    [Change] clear test view

--- front/src/views/TestView.vue
+++ front/src/views/TestView.vue
@@ -1,47 +1,13 @@
 <script setup lang="ts">
 
-import { ref } from "vue";
-import MenuComponent from "@/components/MenuComponent.vue";
-
-
-const items = ref([
-  {
-    to: "home",
-    icon: "fa-solid fa-gear",
-    text: "Settings"
-  },
-  {
-    do: () => console.log("hello"),
-    icon: "fa-solid fa-folder",
-    text: "Projects"
-  },
-  {
-    to: "home",
-    do: () => console.log("hi"),
-    icon: "fa-solid fa-folder",
-    text: "Projects"
-  },
-]);
-
 </script>
 
 
 <template>
-  <div class="page">
-    <MenuComponent class="menu" :items="items" />
-  </div>
+
 </template>
 
 
-<style scoped>
-.page {
-  position: absolute;
-  inset: 0 0 0 0;
-}
-
-.menu {
-  position: fixed;
-  top: 30vh;
-  left: 55vw;
-}
+<style scoped lang="scss">
+@import "@/assets/main.scss";
 </style>
