242cd3ab855756475a9dd0e3c6f304efc55d55e9
TNThung <hung11112002@gmail.com>
2022-07-25 12:42:57 +0000

    [Add] overlay component

---
+++ front/src/components/OverlayComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+  <div class="overlay" />
+</template>
+
+
+<style scoped lang="scss">
+@import "@/assets/main.scss";
+
+.overlay {
+  background-color: black;
+
+  .dark & {
+    background-color: white;
+  }
+
+  position: absolute;
+  inset: 0px;
+  opacity: 20%;
+}
+</style>
