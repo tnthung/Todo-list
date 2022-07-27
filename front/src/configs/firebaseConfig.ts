31045d11dbe1bedc6226f0605ec6ed64be21bc40
TNThung <hung11112002@gmail.com>
2022-07-27 08:35:05 +0000

    [Change] add firebase auth module

--- front/src/configs/firebaseConfig.ts
+++ front/src/configs/firebaseConfig.ts
@@ -1,6 +1,7 @@
 // the fire base tokens
 import configs from "./_firebaseConfig";
 
+import { getAuth } from "firebase/auth"
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
@@ -9,4 +10,5 @@ import { initializeApp } from "firebase/app";
 export const app = initializeApp(configs);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
