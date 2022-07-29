c48df7fafafa6c263e73a8184d4e1730ae234042
TNThung <hung11112002@gmail.com>
2022-07-29 00:34:50 +0000

    [Change] remove firestore

--- front/src/configs/firebaseConfig.ts
+++ front/src/configs/firebaseConfig.ts
@@ -3,12 +3,10 @@ import configs from "./_firebaseConfig";
 
 import { getAuth } from "firebase/auth"
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 
 export const app = initializeApp(configs);
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
